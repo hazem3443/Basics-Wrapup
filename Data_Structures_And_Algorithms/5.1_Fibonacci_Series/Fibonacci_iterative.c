f0a74f6dca7530f622cc5cb0f1e9d7f60701cf2e
hazem3443 <hazemkhaled3443@gmail.com>
2021-07-18 13:16:22 +0200

    Fibonacci Series

--- Data_Structures_And_Algorithms/5.1_Fibonacci_Series/Fibonacci_iterative.c
+++ Data_Structures_And_Algorithms/5.1_Fibonacci_Series/Fibonacci_iterative.c
@@ -11,22 +11,33 @@ int factorial(int n)
 
 int fibbonacci(int n)
 {
-    int f[n + 2];
-    int i;
+    // int f[n + 2];
+    // int i;
 
-    // 0th and 1st number of the
-    // series are 0 and 1
-    f[0] = 0;
-    f[1] = 1;
+    // // 0th and 1st number of the
+    // // series are 0 and 1
+    // f[0] = 0;
+    // f[1] = 1;
 
-    int fib = 0;
-    if (n == 1)
-        fib = 1;
+    // int fib = 0;
+    // if (n == 1)
+    //     fib = 1;
 
-    for (int i = 2; i <= n; i++)
-        f[i] = f[i - 1] + f[i - 2];
+    // for (int i = 2; i <= n; i++)
+    //     f[i] = f[i - 1] + f[i - 2];
 
-    return f[n];
+    // return f[n];
+
+    int a = 0, b = 1, c, i;
+    if (n == 0)
+        return a;
+    for (i = 2; i <= n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
 
 int main()
